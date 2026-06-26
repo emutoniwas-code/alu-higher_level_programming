34ad0af00d3da65e93e25c7fd5b905a5d3b4760b
emutoniwas-code <e.mutoniwas@alustudent.com>
2026-06-26 03:23:14 -0700

    7-islower.py: handle edge cases for empty string and non-string input

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     """Check for lowercase character"""
+    if type(c) is not str or len(c) == 0:
+        return False
     return ord("a") <= ord(c) <= ord("z")
